10d02e4c4968da7f80b4889a561a6ed1e740c34e
tanabe-hideyuki <hideyuki.tanabe@jspnet.co.jp>
2018-09-05 09:24:15 +0900

    my first commit

---
+++ orange.py
@@ -0,0 +1,10 @@
+class Orange:
+    def __init__(self,w,c):
+        self.weight = w
+        self.color = c
+        print("Created!")
+
+or1 = Orange(10,"dark orange")
+print(or1.weight)
+print(or1.color)
+print(or1.weight)
